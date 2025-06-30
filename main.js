3de171d4b8ffa1dbd79e0865e87085edad192dc5
AnhTung-it-03 <Pyndaica000@gmail,com>
2025-06-30 08:29:39 +0700

    update

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
-let n = null;
-do{
-     n = +prompt("enter n : ");
-}while(n>10 || n<1)
-alert("số vừa nhập là " + n);
+function play() {
+     let n = +prompt("Nhập số lớn nhất bạn muốn: ");
+     let b = Math.floor(Math.random() * n); // số bất kỳ
+     let i = 1;
+     let a = +prompt("Lượt 1: Nhập số bạn đoán");
+
+     while (i <= 3) {
+          if (a === b) {
+               document.writeln("Wow! Bạn đoán đúng rồi, thông minh quá!<br>");
+               return;
+          } else {
+               if (a < b) {
+                    a = +prompt("Sai rồi! Nhập số lớn hơn:");
+               } else {
+                    a = +prompt("Sai rồi! Nhập số bé hơn:");
+               }
+          }
+          i++;
+          if (i <= 3 && a !== b) {
+               document.writeln("Lượt đoán thứ " + i + "<br>");
+          }
+     }
+     document.writeln("ất tiếc, bạn đã hết lượt. Số đúng là: " + b);
+}
